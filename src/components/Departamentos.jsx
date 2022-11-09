f0f469542a98c878ec0d36a53f92f17c09473c8a
Gerson <gerson_silguero@outlook.com>
2022-11-08 19:11:23 -0600

    Se agrego el listado con las redirecciones y se editaron los documentos de la carpeta componets

--- src/components/Departamentos.jsx
+++ src/components/Departamentos.jsx
@@ -1,11 +1,109 @@
-import React from "react";
+import logo from '.././logo.svg';
+import '../App.css';
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Outlet, Link } from "react-router-dom";
+import {Container, FormGroup, Table, Button, Modal, ModalHeader, ModalBody, ModalFooter} from 'reactstrap';
 
-const Departamentos = () =>{
-    return(
-        <div>
-            <h1>tabla de departamentos</h1>
-        </div>
-    )
+class App extends React.Component {
+  //Seteamos un estado de la modal por defecto
+  state={
+    modalInsertar: false
+  }
+
+  //esta funcion cambia el estado a true
+  modalInsertar=()=>{
+    this.setState({
+      modalInsertar:true,
+    });
+  }
+
+  //esta funcion cierra la modal
+  cerrarModalInsertar=()=>{
+    this.setState({
+      modalInsertar:false
+      
+    })
+  }
+  onInsertDepto=()=>{
+    let depInfo={
+      nombre:this.refs.nomDep.value
+    };
+
+    console.log(depInfo);
+
+    fetch('http://localhost:5218/api/InsertDepartamento',{
+      method:"POST",
+      headers:{'Content-type':'application/json'},
+      body:depInfo
+    }).then(r=>r.json()).then(res=>{
+      if(res){
+        this.state({message:"Registro creado"});
+      }
+    }) //verificar endpoint
+  }
+  render(){
+    return (
+      <>
+      <nav>
+        <ul>
+          <li>
+            <Link to="/">Departamentos</Link>
+          </li>
+          <li>
+            <Link to="/Empleados">Empleados</Link>
+          </li>
+          <li>
+            <Link to="/Perfiles">Perfiles</Link>
+          </li>
+          <li>
+            <Link to="/Personas">Personas</Link>
+          </li>
+          <li>
+            <Link to="/Usuarios">Usuarios</Link>
+          </li>
+        </ul>
+      </nav>
+
+      <Outlet />
+
+        <Container>
+          <h1>Administración de Departamentos</h1>
+          <br/>
+          <Button color='success' onClick={()=>this.modalInsertar()}>Nuevo Departamento</Button>
+          <br/><br/>
+          <Table className="table-bordered">
+            <thead>
+              <tr align="center">
+                <th width="10%">Id</th>
+                <th>Nombre</th>
+                <th width="20%">Registro</th>
+                <th width="10%">Status</th>
+                <th width="12%">Usuario</th>
+              </tr>
+            </thead>
+          </Table>
+          <Modal isOpen={this.state.modalInsertar}>
+            <ModalHeader>
+              <div>
+                <h3>Departamentos | Nuevo Registro</h3>
+              </div>
+            </ModalHeader>
+            <ModalBody>
+              <FormGroup>
+                <label>Nombre:</label>
+                <input type="text" className="form-control" ref="nomDep"  name="nombre" />
+              </FormGroup>
+            </ModalBody>      
+            <ModalFooter>
+              <Button color="primary" onClick={()=>this.onInsertDepto()}>Guardar Registro</Button>
+              <Button color='danger' onClick={()=>this.cerrarModalInsertar()}>Cancelar</Button>
+            </ModalFooter>
+          </Modal>
+        </Container>
+      </>
+    );
+  }
 }
 
-export default Departamentos
+export default App;
