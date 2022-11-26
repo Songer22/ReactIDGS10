5e187d83c60b54c8fd8e4d3c166e13384091de62
Gerson <gerson_silguero@outlook.com>
2022-11-25 21:45:37 -0600

    actualizacion

--- src/components/Departamentos.jsx
+++ src/components/Departamentos.jsx
@@ -17,17 +17,55 @@ class App extends React.Component {
   componentDidMount(){
     this.cargarTabla();
 
-    function deleteRow(){
-      console.log("hola");
-    }
+    // Delete row
+    $('#lista_deptos').on( 'click', 'tbody td.row-remove', function (e) {
+      var id = e.target.parentNode.parentNode.firstElementChild.textContent;
+      console.log(id);
 
-  //   // Delete row
-  //   $('#lista_deptos').on( 'click', 'tbody td.row-remove', function (e) {
-  //     var id = e.target.parentNode.parentNode.lastElementChild.parentElement.id;
-  //     // var id = e.target.parentElement.id;
-  //     console.log(id);
-  //     // console.log("hola");
-  // } );
+      //headers de la solicitud
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type","application/json")
+      //armar body de la solicitud
+      var data = JSON.stringify({
+        "id":id
+      });
+
+      var requestOptions={
+        method:"Delete",
+        headers:myHeaders,
+        body:data,
+        redirect:"follow"
+      };
+
+      fetch('https://localhost:7218/api/DeleteDepartamento',requestOptions)
+      .then(response=>response.text())
+      .then(result=>console.log(result))
+      .catch(error=>console.log('error', error));
+      Swal.fire(
+        {
+          title:'Se ha eliminado un registro',
+          icon:'success',
+          showConfirmButton: false,
+          timer: 1500
+        }
+    )
+      $('#lista_deptos').DataTable().ajax.reload();
+    } );
+
+    $('#lista_deptos').on( 'click', 'tbody td.row-edit', function (e) {
+      var id = e.target.parentNode.parentNode.firstElementChild.textContent;
+      console.log(id); 
+      
+      this.state={
+        modalEditar: true
+      }
+    }
+    );
+
+    // function ModalEdit(){
+    //   console.log("hola");
+    // }
+    
   }
 
   //Seteamos un estado de la modal por defecto
@@ -35,10 +73,22 @@ class App extends React.Component {
     modalInsertar: false
   }
 
+  //Seteamos un estado de la modal edit por defecto
+  state={
+    modalEditar: false
+  }
+
   //esta funcion cambia el estado a true
   modalInsertar=()=>{
     this.setState({
       modalInsertar:true,
+    });
+  }
+
+   //esta funcion cambia el estado a true Modal Edit
+   modalEditar=()=>{
+    this.setState({
+      modalEditar:true,
     });
   }
 
@@ -128,44 +178,26 @@ class App extends React.Component {
           "data":"Usuario"
         },
         {
-          // data: null,
-          // defaultContent: '<button class="btn btn-danger">Eliminar</button>',
-          // className: 'row-remove dt-center',
-          // orderable: false
-          
           "data": null,
-        "bSortable": false,
-        className: 'row-remove dt-center',
-        "mRender": function(data, type, value) {
-          // console.log(value);
-            // return '<button class="btn btn-danger" onclick="deleteRow('+value["Id"]+')">Eliminar</button>'
-            return '<button class="btn btn-danger row-remove" id="'+value["Id"]+'" onClick={deleteRow}>Eliminar</button>'
-        }
+          "bSortable": false,
+          className: 'row-edit dt-center',
+          "mRender": function(data, type, value) {
+            return '<button class="btn btn-warning row-edit">Editar</button>'
+          }
+        },
+        {  
+          "data": null,
+          "bSortable": false,
+          className: 'row-remove dt-center',
+          "mRender": function(data, type, value) {
+            return '<button class="btn btn-danger row-remove">Eliminar</button>'
+          }
         },
       ]
     });
 
   }
-
-  // deleteRow=()=>{
-  //   console.log("hola");
-  // }
-
-
-//   $('#lista_deptos').on( 'click', 'tbody td.row-remove', function (e) {
-//     editor.remove( this.parentNode, {
-//         title: 'Delete record',
-//         message: 'Are you sure you wish to delete this record?',
-//         buttons: 'Delete'
-//     } );
-// } );
-
- 
-
-  // https://datatables.net/forums/discussion/57742/add-custom-buttons-in-react-datatable
   
-
-
   render(){
     return (
       <>
@@ -185,10 +217,12 @@ class App extends React.Component {
                 <th width="20%">Registro</th>
                 <th width="10%">Status</th>
                 <th width="12%">Usuario</th>
+                <th width="12%">Editar</th>
                 <th width="12%">Eliminar</th>
               </tr>
             </thead>
           </Table>
+          {/* Este el el modal para agregar un nuevo registro */}
           <Modal isOpen={this.state.modalInsertar}>
             <ModalHeader>
               <div>
@@ -204,6 +238,25 @@ class App extends React.Component {
             <ModalFooter>
               <Button color="primary" onClick={()=>this.onInsertDepto()}>Guardar Registro</Button>
               <Button color='danger' onClick={()=>this.cerrarModalInsertar()}>Cancelar</Button>
+            </ModalFooter>
+          </Modal>
+
+          {/* Este es el modal para editar un nuevo registro */}
+          <Modal isOpen={this.state.modalEditar}>
+            <ModalHeader>
+              <div>
+                <h3>Departamentos | Editar Registro</h3>
+              </div>
+            </ModalHeader>
+            <ModalBody>
+              <FormGroup>
+                <label>Nombre:</label>
+                <input type="text" className="form-control" ref="nomDep" id="nomDep" name="nomDep" />
+              </FormGroup>
+            </ModalBody>      
+            <ModalFooter>
+              <Button color="primary" onClick={()=>this.onEditDepto()}>Actualizar Registro</Button>
+              <Button color='danger' onClick={()=>this.cerrarModalEdit()}>Cancelar</Button>
             </ModalFooter>
           </Modal>
         </Container>
