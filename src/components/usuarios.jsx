8a0265a2fbfc930bf12d3c164ffc2a66d411df9d
Gerson <gerson_silguero@outlook.com>
2022-11-28 21:46:35 -0600

    adsa

--- src/components/usuarios.jsx
+++ src/components/usuarios.jsx
@@ -7,7 +7,19 @@ import {Container, FormGroup, Table, Button, Modal, ModalHeader, ModalBody, Moda
 
 import Menu from "../menu/menu";
 
+import Swal from 'sweetalert2';
+import 'jquery/dist/jquery.min.js';
+import 'datatables.net-dt/css/jquery.dataTables.min.css';
+import 'datatables.net-dt/js/dataTables.dataTables.min.js';
+import $ from 'jquery';
+
 class App extends React.Component {
+
+  componentDidMount(){
+    this.cargarTabla();
+  }
+
+
   //Seteamos un estado de la modal por defecto
   state={
     modalInsertar: false
@@ -27,23 +39,96 @@ class App extends React.Component {
       
     })
   }
-  onInsertDepto=()=>{
-    let depInfo={
-      nombre:this.refs.nomDep.value
-    };
-
-    console.log(depInfo);
-
-    fetch('http://localhost:5218/api/InsertDepartamento',{
+  onInsertPerfil=()=>{
+    var nomPerfil = document.getElementById("nomPerfil").value;
+    if (nomPerfil == "") {
+      Swal.fire("Campos vacios","Debes ingresar los datos solicitados","error")
+    }else{
+      //headers de la solicitud
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type","application/json")
+      //armar body de la solicitud
+      var raw = JSON.stringify({
+        "nombre":nomPerfil,
+        "activo":1,
+        "usuario":1
+      });
+    }
+    //options del request
+    var requestOptions={
       method:"POST",
-      headers:{'Content-type':'application/json'},
-      body:depInfo
-    }).then(r=>r.json()).then(res=>{
-      if(res){
-        this.state({message:"Registro creado"});
+      headers:myHeaders,
+      body:raw,
+      redirect:"follow"
+    };
+    fetch('https://localhost:7218/api/InsertPerfil',requestOptions)
+    .then(response=>response.text())
+    .then(result=>console.log(result))
+    .catch(error=>console.log('error', error));
+    this.cerrarModalInsertar();
+    Swal.fire(
+      {
+        title:'Se ha agregado un nuevo registro',
+        icon:'success',
+        showConfirmButton: false,
+        timer: 1500
       }
-    }) //verificar endpoint
+  )
+    $('#lista_usuarios').DataTable().ajax.reload();
   }
+
+
+
+  /* funcion para traer los datos de la tabla con datatable */
+  cargarTabla=()=>{ 
+    $('#lista_usuarios').dataTable().fnDestroy();
+    $('#lista_usuarios').dataTable({
+      "language":{
+        "url":""
+      },
+      "dom":"Bfrtip",
+      "ajax":{
+        "url":"https://localhost:7218/api/GetUsuarios",
+        "method":"POST",
+        "timeout":0,
+        "contentType":"application/json",
+        "dataSrc":"response.data",
+        data:function(d){
+          return JSON.stringify({"activo":0});
+        },
+        dataType:"json"
+      },
+
+      "columns":[
+        {
+          "data":"Id",
+        },
+        {
+          "data":"Nombre"
+        },
+        {
+          "data":"Pass"
+        },
+        {
+          "data":"Id_persona"
+        },
+        {
+          "data":"Id_perfil"
+        },
+        {
+          "data":"Usuario"
+        }
+        {
+          "data":"Activo"
+        },
+        {
+          "data":"Usuario"
+        },
+      ]
+    });
+  }
+
+  
   render(){
     return (
       <>
@@ -54,33 +139,41 @@ class App extends React.Component {
         <Container>
           <h1>Administración de Usuarios</h1>
           <br/>
-          <Button color='success' onClick={()=>this.modalInsertar()}>Nuevo Departamento</Button>
+          <Button color='success' onClick={()=>this.modalInsertar()}>Nuevo Usuario</Button>
           <br/><br/>
-          <Table className="table-bordered">
+          <Table className="table-bordered" id="lista_usuarios">
             <thead>
               <tr align="center">
                 <th width="10%">Id</th>
-                <th>Nombre</th>
-                <th width="20%">Registro</th>
+                <th width="10%">Nombre</th>
+                <th width="10%">pass</th>
+                <th width="10%">Id persona</th>
+                <th width="10%">Id perfil</th>
+                <th width="10%">FechaHora</th>
+                <th width="10%">Status</th>
+                {/* <th>Nombre</th>
+                <th width="20%">Fecha</th>
                 <th width="10%">Status</th>
                 <th width="12%">Usuario</th>
+                <th width="10%">Status</th>
+                <th width="12%">Usuario</th> */}
               </tr>
             </thead>
           </Table>
           <Modal isOpen={this.state.modalInsertar}>
             <ModalHeader>
               <div>
-                <h3>Usuarios | Nuevo Registro</h3>
+                <h3>Perfiles | Nuevo Registro</h3>
               </div>
             </ModalHeader>
             <ModalBody>
               <FormGroup>
                 <label>Nombre:</label>
-                <input type="text" className="form-control" ref="nomDep"  name="nombre" />
+                <input type="text" className="form-control" ref="nomPerfil" id="nomPerfil"  name="nombre" />
               </FormGroup>
             </ModalBody>      
             <ModalFooter>
-              <Button color="primary" onClick={()=>this.onInsertDepto()}>Guardar Registro</Button>
+              <Button color="primary" onClick={()=>this.onInsertPerfil()}>Guardar Registro</Button>
               <Button color='danger' onClick={()=>this.cerrarModalInsertar()}>Cancelar</Button>
             </ModalFooter>
           </Modal>
