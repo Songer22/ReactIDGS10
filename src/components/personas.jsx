38b2a6159203de6a5519a07136d8c2546c8ba348
Gerson <gerson_silguero@outlook.com>
2022-11-30 18:53:32 -0600

    actualizacion

--- src/components/personas.jsx
+++ src/components/personas.jsx
@@ -7,7 +7,19 @@ import {Container, FormGroup, Table, Button, Modal, ModalHeader, ModalBody, Moda
 
 import Menu from "../menu/menu";
 
+import Swal from 'sweetalert2';
+import 'jquery/dist/jquery.min.js';
+import 'datatables.net-dt/css/jquery.dataTables.min.css';
+import 'datatables.net-dt/js/dataTables.dataTables.min.js';
+import $ from 'jquery';
+
 class App extends React.Component {
+
+  componentDidMount(){
+    this.cargarTabla();
+  }
+
+
   //Seteamos un estado de la modal por defecto
   state={
     modalInsertar: false
@@ -27,23 +39,115 @@ class App extends React.Component {
       
     })
   }
-  onInsertDepto=()=>{
-    let depInfo={
-      nombre:this.refs.nomDep.value
-    };
+  onInsertPersona=()=>{
+    var nombre = document.getElementById("nombre").value;
+    var ap_paterno = document.getElementById("ap_paterno").value;
+    var ap_materno = document.getElementById("ap_materno").value;
+    var fecha_nacimiento = document.getElementById("fecha_nacimiento").value;
+    var curp = document.getElementById("curp").value;
+    var email = document.getElementById("email").value;
+    var telefono = document.getElementById("telefono").value;
+    var celular = document.getElementById("celular").value;
 
-    console.log(depInfo);
+    if (nombre == "" || ap_paterno == "" || ap_materno == "" || fecha_nacimiento == "" || curp == "" || email == "" || telefono == "" || celular == "") {
+      Swal.fire("Campos vacios","Debes ingresar los datos solicitados","error")
+    }else{
+      //headers de la solicitud
+      var myHeaders = new Headers();
+      myHeaders.append("Content-Type","application/json")
+      //armar body de la solicitud
+      var raw = JSON.stringify({
+        "nombre":nombre,
+        "ap_paterno":ap_paterno,
+        "ap_materno":ap_materno,
+        "fecha_nacimiento":fecha_nacimiento,
+        "curp":curp,
+        "email":email,
+        "telefono":telefono,
+        "celular":celular,
+        "activo":1,
+        "usuario":1
+      });
 
-    fetch('http://localhost:5218/api/InsertDepartamento',{
+      //options del request
+    var requestOptions={
       method:"POST",
-      headers:{'Content-type':'application/json'},
-      body:depInfo
-    }).then(r=>r.json()).then(res=>{
-      if(res){
-        this.state({message:"Registro creado"});
+      headers:myHeaders,
+      body:raw,
+      redirect:"follow"
+    
+    };
+    fetch('https://localhost:7218/api/InsertPersonas',requestOptions)
+    .then(response=>response.text())
+    .then(result=>console.log(result))
+    .catch(error=>console.log('error', error));
+    this.cerrarModalInsertar();
+    Swal.fire(
+      {
+        title:'Se ha agregado un nuevo registro',
+        icon:'success',
+        showConfirmButton: false,
+        timer: 1500
       }
-    }) //verificar endpoint
+  )
+    $('#lista_Personas').DataTable().ajax.reload();
+
+    }
+    
   }
+
+
+
+  /* funcion para traer los datos de la tabla con datatable */
+  cargarTabla=()=>{ 
+    $('#lista_Personas').dataTable().fnDestroy();
+    $('#lista_Personas').dataTable({
+      "language":{
+        "url":""
+      },
+      "dom":"Bfrtip",
+      "ajax":{
+        "url":"https://localhost:7218/api/GetPersonas",
+        "method":"POST",
+        "timeout":0,
+        "contentType":"application/json",
+        "dataSrc":"response.data",
+        data:function(d){
+          return JSON.stringify({"activo":1});
+        },
+        dataType:"json"
+      },
+
+      "columns":[
+        {
+          "data":"Id",
+        },
+        {
+          "data":"Nombre",
+        },
+        {
+          "data":"Fecha_nacimiento",
+        },
+        {
+          "data":"Curp",
+        },
+        {
+          "data":"Email",
+        },
+        {
+          "data":"Telefono",
+        },
+        {
+          "data":"Celular",
+        },
+        {
+          "data":"Activo",
+        }
+      ]
+    });
+  }
+
+  
   render(){
     return (
       <>
@@ -54,33 +158,58 @@ class App extends React.Component {
         <Container>
           <h1>Administración de Personas</h1>
           <br/>
-          <Button color='success' onClick={()=>this.modalInsertar()}>Nuevo Departamento</Button>
+          <Button color='success' onClick={()=>this.modalInsertar()}>Nuevo Persona</Button>
           <br/><br/>
-          <Table className="table-bordered">
+          <Table className="table-bordered" id="lista_Personas">
             <thead>
               <tr align="center">
                 <th width="10%">Id</th>
                 <th>Nombre</th>
-                <th width="20%">Registro</th>
-                <th width="10%">Status</th>
-                <th width="12%">Usuario</th>
+                <th>Fecha nacimiento</th>
+                <th>Curp</th>
+                <th>Email</th>
+                <th>Telefono</th>
+                <th>Celular</th>
+                <th>Activo</th>
               </tr>
             </thead>
           </Table>
           <Modal isOpen={this.state.modalInsertar}>
             <ModalHeader>
               <div>
-                <h3>Personas | Nuevo Registro</h3>
+                <h3>Persona | Nuevo Registro</h3>
               </div>
             </ModalHeader>
             <ModalBody>
               <FormGroup>
                 <label>Nombre:</label>
-                <input type="text" className="form-control" ref="nomDep"  name="nombre" />
+                <input type="text" className="form-control" ref="nombre" id="nombre"  name="nombre" />
+                <br />
+                <label>Apellido paterno:</label>
+                <input type="text" className="form-control" ref="ap_paterno" id="ap_paterno"  name="ap_paterno" />
+                <br />
+                <label>Apellido materno:</label>
+                <input type="text" className="form-control" ref="ap_materno" id="ap_materno"  name="ap_materno" />
+                <br />
+                <label>Fecha nacimiento:</label>
+                <input type="date" className="form-control" ref="fecha_nacimiento" id="fecha_nacimiento"  name="fecha_nacimiento" />
+                <br />
+                <label>Curp:</label>
+                <input type="text" className="form-control" ref="curp" id="curp"  name="curp" />
+                <br />
+                <label>Email:</label>
+                <input type="text" className="form-control" ref="email" id="email"  name="email" />
+                <br />
+                <label>Telefono:</label>
+                <input type="number" className="form-control" ref="telefono" id="telefono"  name="telefono" />
+                <br />
+                <label>Celular:</label>
+                <input type="number" className="form-control" ref="celular" id="celular"  name="celular" />
+                <br />
               </FormGroup>
             </ModalBody>      
             <ModalFooter>
-              <Button color="primary" onClick={()=>this.onInsertDepto()}>Guardar Registro</Button>
+              <Button color="primary" onClick={()=>this.onInsertPersona()}>Guardar Registro</Button>
               <Button color='danger' onClick={()=>this.cerrarModalInsertar()}>Cancelar</Button>
             </ModalFooter>
           </Modal>
